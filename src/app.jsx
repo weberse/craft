75ad7044d8bebf90ef6689e2ed43623bd5d588fa
Den <dfitsak@gmail.com>
2015-05-14 00:41:27 +0200

    init react router

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,20 @@
 var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
 var Main = require('./components/main');
+var App = require('./components/app');
+var Contact = require('./components/index/footer');
 
-React.render(<Main />,  document.getElementById('app'));
+var routes = (
+    <Route handler={App}>
+        <Route name="page1" handler={Main} />
+        <Route name="page2" handler={Contact} />
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+});
+
+//React.render(<Main />,  document.getElementById('app'));
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,14 @@
 var React = require('react');
-var Header = require('./header');
-var Section = require('./section');
-var Aside = require('./aside');
-var Footer = require('./footer');
+
+var Header = require('./index/header');
+var Section = require('./index/section');
+var Aside = require('./index/aside');
+var Footer = require('./index/footer');
 
 var Main = React.createClass({
     render: function() {
         return (
             <div>
-                <Header />
                 <Section />
                 <Aside />
                 <Footer />
