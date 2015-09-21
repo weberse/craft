d7deb402fbc773f55bfc7ce3659f9e988706d014
Den <dfitsak@gmail.com>
2015-09-21 13:18:03 +0200

    add pure css

--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,21 @@ var React = require('react');
 var Header = React.createClass({
     render: function() {
         return (
-            <header className="row">
-                <div className="container">
-                    <div className="col span_3">
-                        <h2>Web page</h2></div>
-                    <div className="col span_4">
-                        <nav>
-                            <a href="#">Featers</a>
-                            <a href="#">Contact</a>
-                            <a href="#">Docs</a>
-                            <a href="#">Support</a>
-                        </nav>
-                    </div>
-                    <div class="right">
-                        <a href="#" className="button">Login</a>
-                        <a href="#" className="button btn-border">Sign Up</a>
-                    </div>
+            <header className="pure-g">
+                <div className="pure-u-1 pure-u-md-1-3">
+                    <h2>Web page</h2>
+                </div>
+                <div className="pure-u-1 pure-u-md-1-3">
+                    <nav>
+                        <a href="#">Featers</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Docs</a>
+                        <a href="#">Support</a>
+                    </nav>
+                </div>
+                <div className="pure-u-1 pure-u-md-1-3">
+                    <a href="#" className="button">Login</a>
+                    <a href="#" className="button btn-border">Sign Up</a>
                 </div>
             </header>
         );
