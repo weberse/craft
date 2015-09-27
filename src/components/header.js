932c1a701c1a3b34f1bb817f89f6d95964e8eeb8
fden <dfitsak@gmail.com>
2015-09-27 15:45:52 +0200

    one page layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from './Menu';
+import { Router, Route, Link } from 'react-router'
 
 var Header = React.createClass({
     render: function() {
@@ -7,9 +8,9 @@ var Header = React.createClass({
             <header style={this.props.style}>
                 <div className="content pure-g">
                     <div className="pure-u-1 pure-u-md-1-3">
-                        <div className="pure-menu" style={this.props.style.brand}>
+                        <Link to=''><div className="pure-menu" style={this.props.style.brand}>
                             Web page
-                        </div>
+                        </div></Link>
                     </div>
                     <div className="pure-u-1 pure-u-md-2-3">
                         <Menu menu={this.props.menuContent}/>
